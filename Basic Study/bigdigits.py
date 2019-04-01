b88abd8ce98c52ad7840f22120f2e185fc3c20e3
ZombieIce <stephenzhangbupt@me.com>
2019-04-01 15:11:39 +0800

    two little programs

--- Basic Study/bigdigits.py
+++ Basic Study/bigdigits.py
@@ -1,13 +1,32 @@
 import sys
 
-zero = [' *** ', ' * * ',  ' * * ',  ' * * '' *** ']
-one = ['   * ', '   * ', '   * ', '   * ', '   * ']
-two = [' *** ', '   * ', ' *** ', ' *   ', ' *** ']
-three = [' *** ', '   * ', ' *** ', '   * ', ' *** ']
-four = [' * * ', ' * * ', ' *** ', '   * ', '   * ']
-five = [' *** ', ' *   ', ' *** ', '   * ', ' *** ']
-six = [' *   ', ' *   ', ' *** ', ' * * ', ' *** ']
-seven = [' *** ', '   * ', '   * ', '   * ', '   * ']
-eight = [' *** ', ' * * ', ' *** ', ' * * ', ' *** ']
-nine = [' *** ', ' * * ', ' *** ', '   * ', '   * ']
+zero = [' * * * ', ' *   * ',  ' *   * ',  ' *   * ', ' * * * ']
+one = ['     * ', '     * ', '     * ', '     * ', '     * ']
+two = [' * * * ', '     * ', ' * * * ', ' *     ', ' * * * ']
+three = [' * * * ', '     * ', ' * * * ', '     * ', ' * * * ']
+four = [' *   * ', ' *   * ', ' * * * ', '     * ', '     * ']
+five = [' * * * ', ' *     ', ' * * * ', '     * ', ' * * * ']
+six = [' *     ', ' *     ', ' * * * ', ' *   * ', ' * * * ']
+seven = [' * * * ', '     * ', '     * ', '     * ', '     * ']
+eight = [' * * * ', ' *   * ', ' * * * ', ' *   * ', ' * * * ']
+nine = [' * * * ', ' *   * ', ' * * * ', '     * ', '     * ']
+
+Digits = [zero, one, two, three, four, five, six, seven, eight, nine]
+
+try:
+    digits = input('Please input an integer:')
+    row = 0
+    while row < 5:
+        line = ''
+        column = 0
+        while column < len(digits):
+            number = int(digits[column])
+            digit = Digits[number]
+            line += digit[row] + '  '
+            column += 1
+        print(line)
+        row += 1
+except ValueError as err:
+    print(err, 'in', digits)
+
 
