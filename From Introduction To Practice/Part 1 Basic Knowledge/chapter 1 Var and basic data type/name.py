523b6ebe95cb190e92781c5afea101cb49a23864
Stephen Zhang <stephenzhangbupt@me.com>
2019-04-01 23:41:28 +0800

    introduction to practice start!

---
+++ From Introduction To Practice/Part 1 Basic Knowledge/chapter 1 Var and basic data type/name.py
@@ -0,0 +1,23 @@
+name = 'ada lovelance'
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+firstname = 'ada'
+lastname = 'lovelance'
+fullname = firstname + ' ' + lastname
+print(fullname)
+
+print('Hello, ' + fullname.title() + '!')
+message = 'Hello, ' + fullname.title() + '!'
+print(message)
+
+print('\tPython')
+print('Languages:\n\tPython\n\tC\n\tJavaScript')
+
+favorite_language = ' Python '
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
---
+++ From Introduction To Practice/Part 1 Basic Knowledge/chapter 1 Var and basic data type/practice_1.py
@@ -0,0 +1,8 @@
+person = 'Eric'
+print('Hello ' + person + ', would like to learn some python today?')
+print(person.title(), person.lower(), person.upper())
+
+famous_person = 'Albert Einstein'
+message = 'A person who never made a mistake never tried anything new.'
+print(famous_person + ' once said,\" ' + message + '\"')
+
