dee9ea1702ab3ee0699242ef3443cb4a47edfeb0
ZombieIce <stephenzhangbupt@me.com>
2019-04-01 21:02:54 +0800

    complete average1_ans

--- Basic Study/practice/average1_ans.py
+++ Basic Study/practice/average1_ans.py
@@ -1,3 +1,17 @@
 numbers = []
-count = 0
+
+while True:
+    number = input('enter a number or Enter to finish:')
+    if number:
+        try:
+            n = int(number)
+        except ValueError as e:
+            print(e)
+            continue
+        numbers.append(int(n))
+    else:
+        break
+
+print('numbers:', numbers)
+print('count = ', len(numbers), 'sum = ', sum(numbers), 'lowest = ', min(numbers), 'highest = ', max(numbers), 'mean = ', sum(numbers) / len(numbers))
 
