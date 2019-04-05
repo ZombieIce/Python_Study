f107be29c3d0c4f793c6a64efe3b6cccaf6cdcc3
Stephen Zhang <stephenzhangbupt@me.com>
2019-04-06 02:40:54 +0800

    class completed

--- From Introduction To Practice/Part 1 Basic Knowledge/chapter 8 class/electric_car.py
+++ From Introduction To Practice/Part 1 Basic Knowledge/chapter 8 class/electric_car.py
@@ -1,28 +1,26 @@
-"""a class used to describe a car"""
-
-
 class Car(object):
+
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
-        self.odometer_reading = 0
+        self.odometer_reding = 0
 
     def get_descriptive_name(self):
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return long_name
+        return long_name.title()
 
     def read_odometer(self):
-        print('This car has ' + str(self.odometer_reading) + ' miles on it.')
+        print('This car has ' + str(self.odometer_reding) + ' miles on it.')
 
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
+    def update_odometer(self, miles):
+        if miles >= self.odometer_reding:
+            self.odometer_reding = miles
         else:
             print('You can\'t roll back an odometer!')
 
     def increment_odometer(self, miles):
-        self.odometer_reading += miles
+        self.odometer_reding += miles
 
 
 class Battery(object):
