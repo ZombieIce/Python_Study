11e33df07e7e0c45440fe394400602ac7c213a81
ZombieIce <stephenzhangbupt@me.com>
2019-04-08 17:22:26 +0800

    ship start

--- From Introduction To Practice/Part 2 Project/alien_invasion/alien_invasion.py
+++ From Introduction To Practice/Part 2 Project/alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
-
+from ship import Ship
+import game_functions as gf
 from settings import Settings
 
 
@@ -10,14 +11,12 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
+    ship = Ship(ai_settings, screen)
+
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-
-        pygame.display.flip()
+        gf.check_evens(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
--- From Introduction To Practice/Part 2 Project/alien_invasion/settings.py
+++ From Introduction To Practice/Part 2 Project/alien_invasion/settings.py
@@ -1,5 +1,6 @@
-class Settings():
+class Settings(object):
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 960
+        self.screen_height = 640
         self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 1.5
